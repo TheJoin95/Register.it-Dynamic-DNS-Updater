22853f18ddb576097748afe26e779c27fbbb39f3
Miki <miki.lombi@gmail.com>
2019-05-21 17:10:59 +0200

    Nodejs Solution

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # Register.it-Dynamic-DNS-Updater
 Allows the dynamic updating of all the existing record, of any type, that is managed by register.it
 
+# 2019 Update
+
+Due to security change on the Register site the python script won't works anymore.
+
+I suggest you to use the new node solution implemented with Puppeter.
+Clone this repo and make an `cd node && npm install`.
+
+Make sure to have Crhome installed and Node v. > 6.
+
+Then you can run the script typing:
+`node index.js --username=USERNAME_HERE --password=SECRET_PWD --domain=DOMAIN --name=RECORD_NAME --value=RECORD_VALUE --ttl=TTL_>_600 --type=RECORD_TYPE
+`
+
+**Note** that RECORD_TYPE can be one of the following values: NS, A, CNAME, MX, TXT, SRV, AAAA.
+
+I made a few test, if you find something strange open an issue, please.
+
+Test case soon.
+
 # Dynamic DNS for register.it
 
 This script make some request to register.it admin pages by using python and requests library. 
---
+++ node/index.js
@@ -0,0 +1,193 @@
+const puppeteer = require('puppeteer-extra');
+const pluginStealth = require("puppeteer-extra-plugin-stealth");
+
+puppeteer.use(pluginStealth());
+
+const args = {};
+process.argv.slice(2).map(function (val) {
+		let splitted = val.split('=');
+		if(splitted[0].search('--') !== -1)
+			args[splitted[0].replace('--', '')] = splitted[1];
+	}
+);
+
+const defaultUA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36';
+const LOGIN_URL = 'https://controlpanel.register.it/index.html';
+const DOMAIN_URL = 'https://controlpanel.register.it/firstLevel/view.html?domain=';
+const DNS_ADVANCED_URL = 'https://controlpanel.register.it/domains/dnsAdvanced.html';
+
+const availableRecordType = [
+	"NS",
+	"A",
+	"CNAME",
+	"MX",
+	"TXT",
+	"SRV",
+	"AAAA"
+];
+
+if(
+		args['username'] == '' || args['username'] == undefined ||
+		args['password'] == '' || args['password'] == undefined ||
+		args['name'] == '' || args['name'] == undefined ||
+		args['value'] == '' || args['value'] == undefined ||
+		args['domain'] == '' || args['domain'] == undefined
+	) {
+	console.log('Error: invalid params. You need to specify: --username, --password, --name, --value, --domain, --type, --ttl');
+	return false;
+}
+
+const domain = args['domain'];
+const record = {};
+
+record['name'] = args['name'];
+record['type'] = args['type'];
+record['value'] = args['value'];
+record['ttl'] = parseInt(args['ttl']);
+
+if(availableRecordType.indexOf(record['type']) === -1) {
+	console.log('Error on type: type not allowed');
+	return false;
+}
+
+if(args['oldvalue'] !== undefined && args['oldvalue'] != '')
+	record['oldValue'] = args['oldvalue'];
+
+if(parseInt(args['ttl']) < 600){
+	console.log("Error on ttl: only ttl > 600");
+	return false;
+}
+
+
+async function inputClear(page, selector) {
+  await page.evaluate(selector => {
+    document.querySelector(selector).value = "";
+  }, selector);
+}
+
+(async () => {
+  const browser = await puppeteer.launch({
+  	defaultViewport: {
+  		width: 1600,
+  		height: 900
+  	}
+	});
+
+  const page = await browser.newPage();
+  await page.setUserAgent(defaultUA);
+  // await page.setJavaScriptEnabled(false);
+  
+  console.log('Loading login page');
+  await page.goto(LOGIN_URL);
+  await page.waitFor(5000);
+
+  await page.click('.text-center.title-xs');
+  await page.waitFor(1000);
+
+  console.log('Faking user interaction..');
+  await page.evaluate(async () => {
+	  await new Promise((resolve, reject) => {
+	  	// Math.floor(Math.random() * 90)
+	  	document.elementFromPoint(90, 80).click();
+	  	var counter = Math.floor(Math.random() * 20) + 10;
+	  	var iterator = 0;
+      var timer = setInterval(() => {
+          window.scrollBy(0, Math.floor(Math.random() * 60) + 40);
+          iterator++;
+          if(iterator >= counter){
+              clearInterval(timer);
+              resolve();
+          }
+      }, 400);
+    });
+  });
+  
+  console.log('Compiling login form data..');
+  await page.type('.standard-login-module [name="userName"]', args['username']);
+  await page.type('.standard-login-module [name="password"]', args['password']);
+  console.log('Submit login form..');
+  await page.click('.welcome-container-block .standard-login-area [type="submit"]');
+  
+  var errorOnLogin = false;
+
+  // await page.screenshot({path: 'before-login.png'});
+  await page.waitForNavigation().then(() => console.log('Logged in succesfully'), (err) => { errorOnLogin = true; console.log('Error on submit');});
+  // await page.screenshot({path: 'login.png'});
+
+  if(errorOnLogin === false) {
+		// await page.setJavaScriptEnabled(true);
+  	console.log('Loading detail page for the domain: ' + domain);
+	  await page.goto(DOMAIN_URL + domain);
+	  await page.waitFor(2000);
+
+	  console.log('Loading dns advanced page..');
+	  await page.goto(DNS_ADVANCED_URL);
+	  await page.waitFor(3000);
+	  // await page.screenshot({path: 'dns-advanced.png'});
+
+	  var indexToUpdate = null;
+	  var recordCounter = await page.$$(".recordName");
+	  recordCounter = recordCounter.length;
+
+	  if(record.oldValue !== undefined && record.oldValue !== "") {
+	  	const recordNames = await page.$$(".recordName");
+	  	const recordValues = await page.$$(".recordValue");
+	  	for(let i in recordNames) {
+	  		let elName = await recordNames[i].getProperty('value');
+	  		elName = await elName.jsonValue();
+	  		let elOldValue = await recordValues[i].getProperty('value');
+	  		elOldValue = await elOldValue.jsonValue();
+	  		
+	  		if(elName == record.name && record.oldValue == elOldValue) {
+	  			indexToUpdate = i;
+
+			  	if(record.value !== undefined){
+			  		await inputClear(page,'[name="recordValue_' + indexToUpdate + '"]');
+			  		await page.type('[name="recordValue_' + indexToUpdate + '"]', record.value);
+			  	}
+			  	
+			  	if(record.type !== undefined){
+			  		await inputClear(page,'[name="recordType_' + indexToUpdate + '"]');
+			  		await page.select('[name="recordType_' + indexToUpdate + '"]', record.type);
+			  	}
+			  	
+			  	if(record.ttl !== undefined){
+			  		await inputClear(page,'[name="recordTTL_' + indexToUpdate + '"]');
+			  		await page.type('[name="recordTTL_' + indexToUpdate + '"]', record.ttl);
+			  	}
+	  			
+	  			break;
+	  		}
+	  	}
+	  }
+
+	  if(indexToUpdate == null) {
+	  	await page.click('.btn.add');
+	  	await page.waitFor(1000);
+
+	  	var newRecordCounter = recordCounter;
+
+	  	await page.type('[name="recordName_' + newRecordCounter + '"]', record.name);
+	  	await page.type('[name="recordValue_' + newRecordCounter + '"]', record.value);
+	  	await page.select('[name="recordType_' + newRecordCounter + '"]', record.type);
+	  	await page.type('[name="recordTTL_' + newRecordCounter + '"]', record.ttl.toString());
+	  }
+		
+		console.log('Updating..');
+	 	await page.click('.submit.btn');
+	 	// await page.screenshot({path: 'before-prinbtn.png', fullPage: true});
+	 	await page.waitFor(1000);
+
+	 	// await page.screenshot({path: 'printbtn.png', fullPage: true});
+	 	await page.click('.pribttn.nm.apply').then(
+	 		() => console.log('Operation succeded'),
+	 		(err) => console.log('Operation failed')
+ 		);
+
+	 	await page.waitFor(2000);
+		// await page.screenshot({path: 'updated.png'});
+  }
+
+  console.log('Window close');
+  await browser.close();
+})();
